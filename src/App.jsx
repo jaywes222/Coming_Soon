356f09c1cba9069dfa187309f4c37e2308ee4dc7
jaywes222 <wesleyjay202@gmail.com>
2025-01-08 15:31:16 +0300

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Logo from '../src/assets/Logo.svg';
+// import Hero from '../src/assets/hero.jpg';
+import Monkey from '../src/assets/Monkey.jpeg';
 
 const App = () => {
   return (
-    <section className="relative flex h-screen w-screen flex-col overflow-hidden bg-[radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]">
+    <section className="relative flex w-full h-screen flex-col overflow-visible bg-[radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]">
       {/* Top Color Bar */}
       <div className="h-2 bg-gradient-45 w-full absolute z-20"></div>
 
@@ -20,17 +22,16 @@ const App = () => {
       </header>
 
       {/* Main Content */}
-      <div className="container mx-auto flex flex-col lg:flex-row px-5">
+      <main className="container mx-auto flex flex-col lg:flex-row px-5">
         {/* Left Content */}
         <div className="flex-1 flex flex-col justify-center items-start">
-          <h1 className="my-4 mt-24 text-center lg:text-left w-full px-2 text-gray-800 text-3xl xl:text-4xl font-extrabold mb-8">
+          <h1 className="my-4 mt-40 text-center lg:text-left w-full text-gray-800 text-3xl xl:text-4xl font-extrabold mb-8">
             Elevate Your Ideas with{' '}
             <span className="text-secondary">SkeptiKrafts</span>
           </h1>
-          <p className="leading-relaxed mb-12 text-normal xl:text-lg text-center lg:text-left w-full px-4 lg:px-0 text-gray-600">
+          <p className="leading-normal mb-12 text-normal xl:text-lg text-center lg:text-left w-full text-gray-600">
             Transform your digital vision into sleek, intuitive, and purposeful
-            experiences. Stay ahead of the curve with the latest in tech,
-            design, and storytelling.
+            experiences. Stay ahead of the curve with the latest in tech, design, and storytelling.
           </p>
 
           {/* Email Signup Form */}
@@ -65,7 +66,7 @@ const App = () => {
           </form>
 
           {/* Call to Action */}
-          <div className="mt-8 flex text-sm border border-gray-200 rounded-full p-2 text-gray-700 mx-auto lg:mx-0">
+          <div className="mt-8 mb-1 flex text-sm border border-gray-200 rounded-full p-2 text-gray-700 mx-auto lg:mx-0">
             <span className="bg-secondary text-white rounded-full px-3 h-6 flex items-center xl:inline">
               Join Now
             </span>
@@ -76,14 +77,59 @@ const App = () => {
         </div>
 
         {/* Right Content */}
-        <div className="flex-1 hidden lg:flex items-center justify-center relative pl-16">
+        <div className="flex-1 flex justify-center items-center px-5 lg:px-10">
           <img
-            src="./skeptikrafts-hero.jpg"
-            className="w-full object-cover relative bottom-0 z-10 mt-32"
+            src={Monkey}
+            className="w-full max-h-[100vh] object-contain"
             alt="SkeptiKrafts Hero Image"
           />
         </div>
-      </div>
+      </main>
+
+      <section className="bg-gray-100">
+        <div className="container mx-auto mt-0 pb-4 md:pb-24 px-5">
+          <h2 className="w-full pt-8 sm:pt-16 text-2xl sm:text-4xl font-bold text-left text-indigo-500 uppercase">
+            Our Features
+          </h2>
+          <p className="w-full mb-8 md:mb-16 text-xl text-gray-500">
+            Find out how we are different than the rest
+          </p>
+
+          {/* Feature 1 */}
+          <div className="block md:flex flex-wrap mb-16 bg-white rounded shadow overflow-hidden">
+            <div className="w-full md:w-1/2">
+              <img src="./stripe.png" alt="Integrate payments" className="w-full" />
+            </div>
+            <div className="w-full md:w-1/2 py-12 md:py-0 flex flex-col justify-center px-8 sm:px-16">
+              <h3 className="text-xl lg:text-3xl font-bold leading-none mb-3 mt-16 text-gray-700 -mt-1">
+                Integrate your own payments
+              </h3>
+              <p className="text-gray-600 text-xs">
+                You can integrate your Stripe or Braintree payment system right into
+                your course. This means when someone pays for your course, the money
+                will be deposited directly into your account!
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 2 */}
+          <div className="block md:flex flex-wrap mb-16 bg-white rounded shadow overflow-hidden">
+            <div className="w-full md:w-1/2 py-12 md:py-0 flex flex-col justify-center px-8 sm:px-16">
+              <h3 className="text-xl lg:text-3xl font-bold leading-none mb-3 mt-16 text-gray-700 -mt-1">
+                Developer focused courses
+              </h3>
+              <p className="text-gray-600 text-xs">
+                Display code examples and allow users to download files. WeTeach is
+                focused on developers teaching other people to become developers.
+              </p>
+            </div>
+            <div className="w-full md:w-1/2">
+              <img src="./screenshot.png" alt="Developer focused courses" className="w-full" />
+            </div>
+          </div>
+        </div>
+      </section>
+
 
       {/* Footer */}
       <footer className="z-20 pb-10 text-center text-sm font-semibold text-black">
